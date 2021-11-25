faf776b0b49edcf699adfdc6c70e75adef6b89f3
Andrus Naulainen <andrus.naulainen@ametikool.ee>
2021-11-25 14:26:01 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,56 @@
+const cells = document.querySelectorAll("td");
+const gameState = document.getElementById("game-state");
+
+let gameBoard = [
+    ['-', '-', '-'],
+    ['-', '-', '-'],
+    ['-', '-', '-'],
+];
+
+let isX = true;
+let isOver = false;
+
+cells.forEach(cell => {
+    cell.addEventListener('click', e => {
+
+        if ( !isOver ) {
+            if ( cell.innerText == '' ) {
+                if ( isX ) {
+                    cell.innerText = 'X';
+                    gameBoard[cell.dataset.row][cell.dataset.col] = 'X';
+                } else {
+                    cell.innerText = 'O';
+                    gameBoard[cell.dataset.row][cell.dataset.col] = 'O';
+                }
+    
+                isX = !isX;
+                cell.classList.remove('active')
+                isOver = isGameOver();
+
+                if ( isOver ) {
+                    gameState.innerText = 'Mäng läbi';
+                }
+            }
+        }
+
+    });
+});
+
+function isGameOver() {
+    let ret = false;
+
+    for( i = 0; i < 3; i++ ) {
+
+        if ( gameBoard[i][0] == gameBoard[i][1] && gameBoard[i][1] == gameBoard[i][2] && gameBoard[i][0] != '-' ) {
+            ret = true;
+        }
+
+        if ( gameBoard[0][i] == gameBoard[1][i] && gameBoard[1][i] == gameBoard[2][i] && gameBoard[0][i] != '-' ) {
+            ret = true;
+        }
+
+    }
+
+    
+    return ret;
+}
